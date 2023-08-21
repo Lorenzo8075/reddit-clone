267addf3be56fd8f6e1db7d91d48ec4cbe809b8e
Lorenzo Jiang <lorenzo.jiang8075@gmail.com>
2023-08-21 09:19:09 -0700

    Delete db file

--- src/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://bknhrmbhntbtswafhrxx.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrbmhybWJobnRidHN3YWZocnh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk3NDY0ODUsImV4cCI6MjAwNTMyMjQ4NX0.LGXwCe6vqL9bsQFseI4h5raA0ywo-3wNlu5DmvTWPQA";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
